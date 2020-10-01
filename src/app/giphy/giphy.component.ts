e181836c04d7ce3e24627c675402ef65f6c4c996
Josephshitandi <jsphshtnd@gmail.com>
2020-10-01 08:53:54 +0300

    Add getTrending function that utilizes property from trending services

--- src/app/giphy/giphy.component.ts
+++ src/app/giphy/giphy.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Giphy } from '../giphy';
+import { TrendingService } from '../trending-services/trending.service'
 
 @Component({
   selector: 'app-giphy',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./giphy.component.css']
 })
 export class GiphyComponent implements OnInit {
+  giphys: Giphy[];
 
-  constructor() { }
+  constructor(public trendingServices: TrendingService) { }
+
+  getTrending(){
+    this.trendingServices.getTrendingGiphy().then(() => {
+        this.giphys = this.trendingServices.giphys;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
   ngOnInit(): void {
+    this.getTrending()
   }
 
 }
